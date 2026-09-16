946bf3bd39af4db7b08896e22241d245ffa7a2ae
Messias <carvalho.manoel@escolar.ifrn.edu.br>
2026-09-16 13:54:20 -0300

    controle de função

--- js/script.js
+++ js/script.js
@@ -1,29 +1,15 @@
-var nome ="messias"
-let nasc = 2006
-const viva = confirm("clique em ok, se estiver viva");
-let altura;
-if(viva){
-    altura = prompt("digite a sua altura");
+function saudacao_a(){
+    alert (`olá fulano, seja bem-vindo`);
 }
-function calcIdade (ano = 2026){
-    let idade = ano - nasc;
-    console.log(`Idade dentro da funcao:${idade}`);
-    let menor;
-    if(idade < 18){
-        let menor =  true;
-    }else{
-        let menor = false;
-    }
-        return [idade, menor];
-}
-let retorno = calcIdade()
-if (retorno[1]){
-    alert(`Idade: ${retorno[0]}. Você é menor de idade`);
-}else{
-    alert(`Idade: ${retorno[0]}. Você é maior de idade`)
-}
-alerta(`Idade fora da funcao: ${calcIdade()}`);
 
-if(viva){
-    altura = prompt("digite a sua altura");
+function saudacao_b(nome){
+    alert (`olá ${nome}, seja bem-vindo`);
+}
+
+function saudacao_c(nome="desconhecido"){
+    alert(`olá ${nome}, seja bem vindo!`)
+}
+
+function saudacao_d(nome="desconhecido"){
+    return `olá ${nome}, seja bem-vindo!`;
 }
